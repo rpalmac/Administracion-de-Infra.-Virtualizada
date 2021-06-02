595bce408ec1ea9bb59534b3489b15d4a89e20eb
Raul Jesus Palma Castilla <70992470+rpalmac@users.noreply.github.com>
2021-06-01 21:20:01 -0500

    Creado con Colaboratory

---
+++ LlamadoDeFunciones.py
@@ -0,0 +1,55 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled1.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyN4+oafxX0f5QQdNAshqJKO",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/rpalmac/Administracion-de-Infra.-Virtualizada/blob/main/LlamadoDeFunciones.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "tCyUDcX4g5Xz"
+      },
+      "source": [
+        "#Declaracion de funciones\n",
+        "\n",
+        "#Funcion sin parametros\n",
+        "\n",
+        "def funcion1 ():\n",
+        "\tprint('Has llamado a la funcion 1')\n",
+        "\n",
+        "#Funcion con parametros y retorno\n",
+        "\n",
+        "def funcion2(a,b):\n",
+        "\t#contenido de la funcion\n",
+        "        #Esto realiza la suma de esta función\n",
+        "\tc=int(a)+int(b)\n",
+        "\treturn c"
+      ],
+      "execution_count": 2,
+      "outputs": []
+    }
+  ]
+}
